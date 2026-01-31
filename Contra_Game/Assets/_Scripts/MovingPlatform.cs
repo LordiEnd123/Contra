61ee5577ba0052ca4fbb2d5b7b179b11efe6ea0a
LordiEnd <tavdaba@gmail.com>
2026-01-31 23:49:07 +1000

    Add platform

---
+++ Contra_Game/Assets/_Scripts/MovingPlatform.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform posA;
+    public Transform posB;
+    public float speed = 3f;
+
+    private Vector3 targetPos;
+
+    void Start()
+    {
+        targetPos = posB.position;
+    }
+
+    void Update()
+    {
+        // Едем к цели
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        // Если приехали — меняем цель
+        if (Vector3.Distance(transform.position, targetPos) < 0.1f)
+        {
+            if (targetPos == posB.position) targetPos = posA.position;
+            else targetPos = posB.position;
+        }
+    }
+
+    // --- МАГИЯ ПРИКЛЕИВАНИЯ ---
+
+    // Когда Игрок наступает на платформу
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Проверяем по тегу, точно ли это Player
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Делаем игрока "пассажиром" (дочерним объектом) платформы
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    // Когда Игрок спрыгивает
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Освобождаем игрока (убираем родителя)
+            collision.transform.SetParent(null);
+        }
+    }
+}
